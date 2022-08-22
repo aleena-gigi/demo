1d2690480f8aa41ba1674bcb4e2b9ae111c0fbbf
aleena-gigi <aleenagigi16@gmail.com>
2022-08-22 05:06:39 -0400

    variable port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("app.js")
 const app=express()
+const port=process.env.port||3000
 app.get("/",(req,res)=>{
     res.send("This is an express application")
-}).listen(3002)
+}).listen(port)
